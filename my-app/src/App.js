54e12381ff7a20c08625ac2c40f0447093620dbd
vitalyvitmens <otm_@tut.by>
2023-07-11 16:49:50 +0300

    add data in project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { Header, UserBlock } from './components'
 import styles from './app.module.css'
 import { AppContext } from './context'
@@ -11,21 +12,67 @@ const getUserFromServer = () => ({
 	phone: '+375297777777',
 })
 
+const getAnotherUserFromServer = () => ({
+	id: 'a77700',
+	name: 'Егор',
+	age: 14,
+	email: 'egor@tut.by',
+	phone: '+375447777777',
+})
+
+const reducer = (state, action) => {
+	const { type, payload } = action
+
+	switch (type) {
+		case 'SET_USER_DATA': {
+			return payload
+		}
+		case 'SET_USER_AGE': {
+			return {
+				...state,
+				age: payload,
+			}
+		}
+		default:
+			return state
+	}
+}
+
 export const App = () => {
-	const userData = getUserFromServer()
+	const [userData, setUserData] = useState({})
+
+	const dispatch = (action) => {
+		const newState = reducer(userData, action)
+
+		setUserData(newState)
+	}
+
+	useEffect(() => {
+		const userDataFromServer = getUserFromServer()
+
+		setUserData(userDataFromServer)
+	}, [])
+
+	const onUserChange = () => {
+		const newUserDataFromServer = getAnotherUserFromServer()
+
+		setUserData(newUserDataFromServer)
+	}
 
 	return (
-		<AppContext.Provider value={userData}>
+		<AppContext.Provider value={{ userData, dispatch }}>
 			<div className={styles.app}>
 				<Header />
 				<hr />
 				<UserBlock />
+				<button onClick={onUserChange}>Сменить пользователя</button>
 			</div>
 		</AppContext.Provider>
 	)
 }
 
-{/* <AppContextProvider.Provider
+{
+	/* <AppContextProvider.Provider
 themeValue={{ theme: 'light' }}
 userValue={userData}
 appConfigValue={null}
@@ -35,4 +82,5 @@ appConfigValue={null}
   <hr />
   <UserBlock />
 </div>
-</AppContextProvider.Provider> */}
+</AppContextProvider.Provider> */
+}
--- my-app/src/components/user-block/components/user-personal-info/user-personal-info.js
+++ my-app/src/components/user-block/components/user-personal-info/user-personal-info.js
@@ -2,12 +2,26 @@ import { useContext } from 'react'
 import { AppContext } from '../../../../context'
 
 export const UserPersonalInfo = () => {
-	const { name, age } = useContext(AppContext)
+	const { userData, dispatch } = useContext(AppContext)
+	const { name, age, email, phone } = userData
+
+	const onUserUpdate = () => {
+		const newUserData = { name, age: 30, email, phone }
+		// setUserData(newUserData)
+		dispatch({ type: 'SET_USER_DATA', payload: newUserData })
+	}
+
+	const onUserAgeDecrease = () => {
+		dispatch({ type: 'SET_USER_AGE', payload: 15 })
+	}
+
 	return (
 		<div>
 			<h3>Персональные данные:</h3>
 			<div>Имя: {name}</div>
 			<div>Возраст: {age}</div>
+			<button onClick={onUserUpdate}>Обновить пользователя</button>
+			<button onClick={onUserAgeDecrease}>Уменьшить возраст пользователя</button>
 		</div>
 	)
 }
